80b57dcd6c193b839371ee8f26f7d00d1fd71808
Yajuddin Raju <yajuddinraju1@gmail.com>
2026-08-14 05:28:19 -0400

    hero section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#fffcf9] text-[#1a1210] font-sans antialiased">
       <Navbar />
-      
-      
+      <main>
+        <Banner />
+      </main>
     </div>
   );
 }
